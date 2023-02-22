1a54892ef8c1b0b2e6159e6f3d2c9e0e18ede5e4
AdmNeil <julien.grit44@gmail.com>
2023-02-22 14:50:46 +0100

    BugFix

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -65,7 +65,7 @@ class SortieType extends AbstractType
                 'attr' => ['class' => "button"]
             ])
             ->add('removeSortie', SubmitType::class, [
-                'label' => 'Supprimer la sortie'
+                'label' => 'Supprimer la sortie',
                 'attr' => ['class' => "button"]
             ])
             ->add('annuleSortie', SubmitType::class, [
